eeead5cb769b25124656c81093152867ab4ad664
Lars <lhs91@outlook.de>
2023-02-17 16:53:38 +0100

    Beschreibung für Angebot hinzugefügt

--- README.md
+++ README.md
@@ -1,26 +1,18 @@
 # Angebot-Lambda
 
-Diese AWS-Lambda-Function dient zur Pflege  von Angeboten einer Applikation. Die Angebote (Betreff + Nachricht) werden in einer DynamoDB mit einer optionalen Gültigkeit hinterlegt. 
+## Routen
 
-Sofern das 'GültigBis' gepflegt ist, werden die Angebote mit einer TTL in der DynamoDB hinterlegt und nach einem Versatz von 7 Tagen zum 'GültigBis' gelöscht (Feature ist über die Umgebungsvariable 'TTL_FEATURE_ACTIVE' deaktivierbar).
+### Angebot 
 
-## Voraussetzungen
-- Python (3.9)
-    - boto3
-    - moto
-    - pytest
-    - aws_lambda_powertools
+- POST api/angebot
+- GET api/angebot/<id>
+- GET api/angebot[?stichtag=YYYY-MM-DD]
+- PUT api/angebot/<id>
+- DELETE api/angebot/<id>
+
 
 ## Umgebungsvariablen
-```sh
-TTL_FEATURE_ACTIVE=0|1:int
-ANGEBOT_TABLE_NAME=*:str
-```
-
-## Tests ausführen
-```sh
-pytest
-```
-
-
-# Api-Dokumentation (wip)
+| Name                   | Beschreibung                                          |
+|------------------------|-------------------------------------------------------|
+| ANGEBOT_TABLE_NAME     | Name der Angebot DynamoDB-Table                       |
+| TTL_FEATURE_ACTIVE     | Flag, ob TTL für die Angebot DynamoDB-Table aktiv ist |
--- tests/test_angebot_lambda_get_all.py
+++ tests/test_angebot_lambda_get_all.py
@@ -57,6 +57,72 @@ def test_get_angebote_by_stichtag_ok(lambda_context, dynamodb_table):
     assert json.dumps(body[0]) == item2023.to_json()
 
 
+def test_get_angebote_by_stichtag_with_angebot_in_future_ok(lambda_context, dynamodb_table):
+    item1 = {
+        'bezeichnung': "Testangebot",
+        "preis": "5.21",
+        "preisEinheit": "€/Stück",
+        "gueltigVon": "2022-01-01",
+        "gueltigBis": "2022-02-01"
+    }
+    item2 = {
+        'bezeichnung': "Testangebot 2",
+        "preis": "9",
+        "preisEinheit": "€/Stück",
+        "gueltigVon": "2023-01-01",
+        "gueltigBis": "2023-02-01"
+    }
+    item3 = {
+        'bezeichnung': "Testangebot 3",
+        "preis": "9",
+        "preisEinheit": "€/Stück",
+        "gueltigVon": "2024-01-01",
+        "gueltigBis": "2024-02-01"
+    }
+    angebot_controller.create_angebot(DEFAULT_TENANT_ID, item1)
+    angebot_controller.create_angebot(DEFAULT_TENANT_ID, item2)
+    angebot_controller.create_angebot(DEFAULT_TENANT_ID, item3)
+
+    response = angebot_handler.handle(
+        event('/api/angebot', 'GET', queryParameters={"stichtag": "2023-01-27"}), lambda_context)
+    body = extract_body(response)
+
+    assert extract_status_code(response) == 200
+    assert len(body) == 2
+
+
+def test_get_angebote_by_stichtag_with_angebot_without_gueltigkeit_ok(lambda_context, dynamodb_table):
+    item1 = {
+        'bezeichnung': "Testangebot",
+        "preis": "5.21",
+        "preisEinheit": "€/Stück",
+        "gueltigVon": "2022-01-01",
+        "gueltigBis": "2022-02-01"
+    }
+    item2 = {
+        'bezeichnung': "Testangebot 2",
+        "preis": "9",
+        "preisEinheit": "€/Stück",
+        "gueltigVon": "2023-01-01",
+        "gueltigBis": "2023-02-01"
+    }
+    item3 = {
+        'bezeichnung': "Testangebot 3",
+        "preis": "9",
+        "preisEinheit": "€/Stück"
+    }
+    angebot_controller.create_angebot(DEFAULT_TENANT_ID, item1)
+    angebot_controller.create_angebot(DEFAULT_TENANT_ID, item2)
+    angebot_controller.create_angebot(DEFAULT_TENANT_ID, item3)
+
+    response = angebot_handler.handle(
+        event('/api/angebot', 'GET', queryParameters={"stichtag": "2023-01-27"}), lambda_context)
+    body = extract_body(response)
+
+    assert extract_status_code(response) == 200
+    assert len(body) == 2
+
+
 def test_get_angebote_empty_ok(lambda_context, dynamodb_table):
     response = angebot_handler.handle(
         event('/api/angebot', 'GET'), lambda_context)
